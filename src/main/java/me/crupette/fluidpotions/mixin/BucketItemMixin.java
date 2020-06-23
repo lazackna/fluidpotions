b66a9ca24b42c9f2c21f587b97769b262cb683d0
Jon Santmyer <jonsantmyer@gmail.com>
2020-06-23 12:22:27 -0400

    fix bucket mixin update to 1.16

--- src/main/java/me/crupette/fluidpotions/mixin/BucketItemMixin.java
+++ src/main/java/me/crupette/fluidpotions/mixin/BucketItemMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.potion.PotionUtil;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,12 +24,13 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public abstract class BucketItemMixin {
 
     @Inject(
-            method = "use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;",
-            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/BucketItem;getFilledStack(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/Item;)Lnet/minecraft/item/ItemStack;"),
+            method = "use",
+            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemUsage;method_30012(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"),
             locals = LocalCapture.CAPTURE_FAILHARD)
     private void useOnPotionFluid(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> ci,
-                                  ItemStack itemStack, BlockHitResult blockHitResult, BlockPos pos, BlockPos pos2, BlockState blockState,
-                                  Fluid fluid, ItemStack itemStack2){
+                                  ItemStack itemStack, BlockHitResult blockHitResult,
+                                  BlockPos blockPos, BlockPos blockPos2,
+                                  BlockState blockState, Fluid fluid, ItemStack itemStack2){
         if(fluid instanceof PotionFluid){
             PotionUtil.setPotion(itemStack2, ((PotionFluid) fluid).getPotion());
         }
